8e74aa2b86bb2d0e1bbfb7179d31377c3257244e
Vincent Wan <vincent.wan@linaro.org>
2019-01-08 11:51:26 -0800

    Update SimpleLink wifi driver to use new SimpleLink SDK
    Changes are made to the driver due to source file and API changes.
    
    user.h is updated for Zephyr to provide its own os_Spawn
    implementation.

--- ext/hal/ti/simplelink/source/ti/drivers/net/wifi/porting/user.h
+++ ext/hal/ti/simplelink/source/ti/drivers/net/wifi/porting/user.h
@@ -1181,9 +1181,11 @@ typedef signed int _SlFd_t;
     
     \warning                User must implement it's own 'os_Spawn' function.
 */
-//#define SL_PLATFORM_EXTERNAL_SPAWN
+/* Zephyr Port provides its own os_Spawn() implementation */
+#define SL_PLATFORM_EXTERNAL_SPAWN
 
 #ifdef SL_PLATFORM_EXTERNAL_SPAWN
+extern  _i16 os_Spawn(P_OS_SPAWN_ENTRY pEntry, void *pValue, unsigned long flags);
 #define sl_Spawn(pEntry,pValue,flags)       os_Spawn(pEntry,pValue,flags)        
 #endif
 
